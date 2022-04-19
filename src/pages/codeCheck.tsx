4a083a6ac695e86593ca033924a9ad000b8f2b8b
pikapika2 <yossii0108@gmail.com>
2022-04-19 17:42:39 +0900

    add check letter
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/codeCheck.tsx
+++ src/pages/codeCheck.tsx
@@ -4,7 +4,23 @@ const CodeCheck: React.FC<Props> = ({ addText, onChange }) => {
   if (addText === 'undefined' || addText.length === 0) {
     return <div>not data</div>
   }
-  return <div> {addText} </div>
+  const testArr = /abc/
+  const result = testArr.test(addText)
+  console.log(result)
+  if (result === true) {
+    return (
+      <home>
+        <div>aaa</div>
+        <div> {addText} </div>
+      </home>
+    )
+  }
+
+  return (
+    <home>
+      <div> {addText} </div>
+    </home>
+  )
 }
 
 export default CodeCheck
