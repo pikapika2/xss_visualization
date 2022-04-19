0670b140f45cde8f7fbee14cebcf25326841c9bc
pikapika2 <yossii0108@gmail.com>
2022-04-19 14:56:06 +0900

    add input parts
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
+import React, { useState } from 'react'
+
 export default function Home() {
+  const [text, setText] = useState('')
+  const [addText, setAddText] = useState('')
+  const onClickAddText = () => {
+    setAddText(text)
+  }
   return (
     <home>
-      <div>サイト１</div>
-      <div>サイト２</div>
+      <div>
+        <p>サイト１</p>
+        <input
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          name="ここにスクリプト混入させたい"
+        />
+        <button onClick={(event) => setAddText(text)}>追加</button>
+        <p>ボタンクリック：{addText}</p>
+      </div>
+      <div>サイト２(一応悪意ある想定、踏むだけで終わる)</div>
     </home>
   )
 }
