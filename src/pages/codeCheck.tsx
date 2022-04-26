deb325cf5ea9a2790304f232935c1904cfc811b5
pikapika2 <yossii0108@gmail.com>
2022-04-26 17:00:48 +0900

    add css class
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/codeCheck.tsx
+++ src/pages/codeCheck.tsx
@@ -3,11 +3,11 @@ import AHref from './Ahref'
 
 const CodeCheck: React.FC<Props> = ({ addText, onChange }) => {
   if (addText === 'undefined' || addText.length === 0) {
-    return <div>not data</div>
+    return <p className="default_text">(ここに入力結果が表示されます)</p>
   }
   const aTag = /<a href="https?:\/\/([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?">/
   const resultATag = aTag.test(addText)
-  console.log(resultATag)
+  //console.log(resultATag)
   if (resultATag === true) {
     return AHref(addText)
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import CodeCheck from './codeCheck'
+import Image from 'next/image'
+import phishingImage from '../image/phishing.png'
 
 export default function Home() {
   const [text, setText] = useState('')
@@ -7,23 +9,29 @@ export default function Home() {
 
   return (
     <React.Fragment>
-      <div className="bulletin_board">
+      <div className="left_side">
+        <div className="bulletin_board">
+          <p className="site_name">
+            掲示板サイト１(この掲示板は対策0でinnerHTMLを使って出力していると想定)
+          </p>
+          <CodeCheck addText={addText} />
+          <input
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+            name="ここにスクリプト混入させたい"
+            maxLength="100"
+          />
+          <button onClick={(event) => setAddText(text)}>これを仕掛ける</button>
+        </div>
+        <p>入力された文字列：{addText}</p>
         <p className="site_name">
-          掲示板サイト１(この掲示板は対策0でinnerHTMLを使って出力していると想定)
+          サイト２(一応悪意ある想定、踏むだけで終わる)(http://example1.com)
+          <br />
         </p>
-        <CodeCheck addText={addText} />
-        <input
-          value={text}
-          onChange={(event) => setText(event.target.value)}
-          name="ここにスクリプト混入させたい"
-          maxLength="100"
-        />
-        <button onClick={(event) => setAddText(text)}>これを仕掛ける</button>
+        <br />
+        <Image className="site_img" src={phishingImage} />
       </div>
-      <p>入力された文字列：{addText}</p>
-      <div className="site_name">
-        サイト２(一応悪意ある想定、踏むだけで終わる)(http://example1.com)
-      </div>
+      <div className="right_side">テスト</div>
     </React.Fragment>
   )
 }
