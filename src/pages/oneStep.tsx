a4d6e500ee97884e17251ec28337e3a32cf7c32f
pikapika2 <yossii0108@gmail.com>
2022-06-24 14:42:19 +0900

    data update
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/oneStep.tsx
+++ src/pages/oneStep.tsx
@@ -1,17 +1,53 @@
 import React, { useState } from 'react'
 import Xarrow from 'react-xarrows'
 
+let count = 0
+
 const OneStep: React.FC<Props> = () => {
   const [show1, setShow1] = useState(false)
   const [show2, setShow2] = useState(false)
   const [show3, setShow3] = useState(false)
 
+  function testShow() {
+    if (count === 0) {
+      setShow1(!show1)
+      count++
+    } else if (count === 1) {
+      setShow1(!show1)
+      setShow2(!show2)
+      count++
+    } else if (count === 2) {
+      setShow2(!show2)
+      setShow3(!show3)
+      count++
+    } else if (count === 3) {
+      setShow3(!show3)
+      count = 0
+    }
+  }
+
   return (
     <React.Fragment>
       <button onClick={() => setShow1(!show1)}>どうなるか</button>
-      <Xarrow start="normal_human" end="dangerousHTML" showXarrow={show1} />
-      <Xarrow start="dangerousHTML" end="normal_human" showXarrow={show2} />
-      <Xarrow start="dangerousWebsite" end="normal_human" showXarrow={show3} />
+      <button onClick={() => testShow()}>テスト</button>
+      <Xarrow
+        start="normal_human"
+        end="dangerousHTML"
+        labels="アクセスしてクリックする"
+        showXarrow={show1}
+      />
+      <Xarrow
+        start="dangerousHTML"
+        end="normal_human"
+        labels="クリック結果が返ってくる"
+        showXarrow={show2}
+      />
+      <Xarrow
+        start="normal_human"
+        end="dangerousWebsite"
+        labels="思ってたのと違うところに行く"
+        showXarrow={show3}
+      />
     </React.Fragment>
   )
 }
