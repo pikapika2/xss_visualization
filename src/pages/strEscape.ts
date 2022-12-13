53355a42fbb8d544776af3d05a1f57e9be6c0e2a
pikapika2 <yossii0108@gmail.com>
2022-12-14 00:32:33 +0900

    add toString
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/strEscape.ts
+++ src/pages/strEscape.ts
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 function strEscape(str: string) {
   return str
+    .toString()
     .replace(/&/g, '&amp;')
     .replace(/"/g, '&quot;')
     .replace(/</g, '&lt;')
